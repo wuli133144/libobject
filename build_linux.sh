1a9ab9800ab188dbabb5a9058aca5d410cb62ad5
unknown <wuyujie@ESG.360ES.CN>
2019-09-04 18:41:08 +0800

    fix CMakeLists.txt to remove UI_MODULE

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-rm -rf CMakeFiles  CMakeCache.txt    cmake_install.cmake 
-cmake . -DPLATFORM=linux -DMODULE_UI=on
+rm -rf CMakeFiles  CMakeCache.txt    cmake_install.cmake  Makefile
+cmake . -DPLATFORM=linux -DMODULE_UI=OFF
 make
 make install
